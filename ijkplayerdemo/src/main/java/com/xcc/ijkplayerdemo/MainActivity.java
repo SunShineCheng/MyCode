23b91cd7fef5b270f4f179176677273d7f26809e
SunShineCheng <715112063@qq.com>
2016-11-11 11:19:06 +0800

    ijkplayer简单用法练习

---
+++ ijkplayerdemo/src/main/java/com/xcc/ijkplayerdemo/MainActivity.java
@@ -0,0 +1,95 @@
+package com.xcc.ijkplayerdemo;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.dou361.ijkplayer.listener.OnShowThumbnailListener;
+import com.dou361.ijkplayer.widget.PlayStateParams;
+import com.dou361.ijkplayer.widget.PlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View rootView;
+    private PlayerView player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        rootView = getLayoutInflater().from(this).inflate(R.layout.simple_player_view_player, null);
+        setContentView(rootView);
+        String url = "http://9890.vod.myqcloud.com/9890_9c1fa3e2aea011e59fc841df10c92278.f20.mp4";
+        player = new PlayerView(this)
+                .setTitle("什么")
+                .setScaleType(PlayStateParams.fitxy)
+                .hideMenu(true)
+                .forbidTouch(false)
+
+                .showThumbnail(new OnShowThumbnailListener() {
+                    @Override
+                    public void onShowThumbnail(ImageView ivThumbnail) {
+//                        Glide.with(mContext)
+//                                .load("http://pic2.nipic.com/20090413/406638_125424003_2.jpg")
+//                                .placeholder(R.color.cl_default)
+//                                .error(R.color.cl_error)
+//                                .into(ivThumbnail);
+                    }
+                })
+                .setPlaySource(url)
+                .startPlay();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player != null) {
+            player.onPause();
+        }
+        /**demo的内容，恢复系统其它媒体的状态*/
+        //MediaUtils.muteAudioFocus(mContext, true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (player != null) {
+            player.onResume();
+        }
+        /**demo的内容，暂停系统其它媒体的状态*/
+        //MediaUtils.muteAudioFocus(mContext, false);
+        /**demo的内容，激活设备常亮状态*/
+        //if (wakeLock != null) {
+        //    wakeLock.acquire();
+        //}
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (player != null) {
+            player.onDestroy();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (player != null) {
+            player.onConfigurationChanged(newConfig);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (player != null && player.onBackPressed()) {
+            return;
+        }
+        super.onBackPressed();
+        /**demo的内容，恢复设备亮度状态*/
+        //if (wakeLock != null) {
+        //    wakeLock.release();
+        //}
+    }
+}
